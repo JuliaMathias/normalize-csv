b6eea491d038a5ef5f67bf1bd46115f295409a2a
JuliaMathias <juliamss@gmail.com>
2022-01-30 10:14:14 -0300

    initial commit

---
+++ lib/csv_test.ex
@@ -0,0 +1,18 @@
+defmodule CsvTest do
+  @moduledoc """
+  Documentation for `CsvTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
